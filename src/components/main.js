1b0a145b844bbb9198384a6fa3ffb7aaa61d82d6
dreinasant <ao.andreina@gmail.com>
2022-06-23 19:53:34 -0300

    mais uma atualização na main

--- src/components/main.js
+++ src/components/main.js
@@ -3,17 +3,18 @@ import styled from "styled-components";
 import FatiaDeBolo from "../imgs/fatia-de-bolo.png";
 import Pizza from  "../imgs/pizza.png";
 import Suco from "../imgs/suco.png";
+import Colher from "../imgs/colher.png";
 
 const Main = styled.main`
 background: #F2F4F1;
-height: 65vw;
+height: 100vw;
 margin-top:1.2vw; 
 `
 const Box = styled.div`
 display: flex;
 align-items: center;
 flex-direction: column;
-transform: translate(0%, 20%);
+transform: translate(0%, 10%);
 `
 const H1 = styled.h1`
 font-family: Hurme Geometric Sans 1;
@@ -103,6 +104,49 @@ margin-top: 0.9vw;
 margin-top: 3vw;
 `
 
+const BoxAbout = styled.div`
+display: flex;
+height: 50vw;
+`
+const ImgSpoon= styled.img`
+width: vw;
+height: 50vw;
+margin-top: 10vw;
+
+`
+const ContainerAbout = styled.div`
+background: white;
+height:45vw;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+margin-top: 10vw;
+
+`
+const H2About = styled.h2`
+font-family: Hurme Geometric Sans 1;
+letter-spacing: 1.72px;
+color: #373737;
+margin-top: 3vw;
+font-size: 1.3em;
+`
+const BarraAbout = styled.div`
+width: 2.5vw;
+border-style: solid;
+border-bottom-width: 0.1px;
+color: black;
+margin-top: 1vw;
+`
+const ParagrafoAbout = styled.p`
+width: 60%;
+font-family: Hurme Geometric Sans 1;
+font-size: 0.8em;
+letter-spacing: 0px;
+color: #373737;
+margin-top: 5vw;
+`
+
 
 export default function App(){
     return(
@@ -129,6 +173,14 @@ export default function App(){
                 <H2Img>Peanut Smoothie</H2Img>
                 </ContainerCopo>
                 </BoxImgs>
+                <BoxAbout>
+                    <ImgSpoon src={Colher} alt="colher"/>
+                    <ContainerAbout>
+                    <H2About>ABOUT</H2About>
+                    <BarraAbout></BarraAbout>
+                    <ParagrafoAbout>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, tortor nec rhoncus dictum, lorem massa tempus sem, <br/>eu tincidunt libero velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum nisl ante, et ultricies sapien facilisis aliquam.</ParagrafoAbout>
+                    </ContainerAbout>
+                </BoxAbout>
             </Box>
            
         </Main>
